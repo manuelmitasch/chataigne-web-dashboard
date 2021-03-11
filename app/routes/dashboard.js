7276c6f4d01dabd9323c47fb232a6ba7fa999ed3
Manuel Mitasch <manuel@mitasch.com>
2021-03-11 23:27:13 +0100

    Redirect to first dashboard if url dashboard is not found

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,6 +2,13 @@ import Route from '@ember/routing/route';
 
 export default class DashboardRoute extends Route {
     model(params) {
-        return this.store.peekRecord('dashboard', params.dashboard_id)
+      return this.store.peekRecord('dashboard', params.dashboard_id)
+    }
+  
+    afterModel(model, transition) {
+      if (model == null) {
+        let firstDashboard = this.store.peekAll('dashboard').firstObject;
+        this.transitionTo('dashboard', firstDashboard);
       }
+    }
 }
