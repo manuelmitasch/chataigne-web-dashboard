f8a4aae880c5dcd27ee41307f2e5ad4a66978296
Manuel Mitasch <manuel@mitasch.com>
2021-03-16 15:22:10 +0100

    Scale font size of trigger based on height.

--- app/components/trigger-control.js
+++ app/components/trigger-control.js
@@ -11,4 +11,28 @@ export default class TriggerControlComponent extends ControlComponent {
     event.preventDefault();
     // control.save();
   }
+
+  get customizeStyles() {
+    let control = this.args.control;
+    let h = control.height;
+    let fontSize = 16;
+
+    if (h <= 15) {
+      fontSize = 10;
+    } else if (h <= 25) {
+      fontSize = 12;
+    } else if (h <= 35) {
+      fontSize = 14;
+    } else if (h <= 45) {
+      fontSize = 16;
+    } else {
+      fontSize = 18;
+    } 
+
+    let styles = {
+      fontSize: fontSize + "px"
+    };
+
+    return styles;
+  }
 }
