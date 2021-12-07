a739bcb9678b804e89a3adfdcc36d0bec7e18545
Manuel Mitasch <manuel@mitasch.com>
2021-12-07 01:02:01 +0100

    Add error when no texture was received.

--- app/components/texture.js
+++ app/components/texture.js
@@ -3,6 +3,8 @@ import { action } from '@ember/object';
 
 export default class TextureControlComponent extends ControlComponent {  
   get srcBlob() {
-    return 'data:image/jpeg;base64,' + this.args.control.imgBlob;
+    if (this.args.control.imgBlob) {
+      return 'data:image/jpeg;base64,' + this.args.control.imgBlob;
+    }
   }
 }
